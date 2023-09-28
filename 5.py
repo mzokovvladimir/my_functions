49c7b04b9691d26597f5ee6b3cd3c8b77a8cbbe6
mzokovvladimir <105171108+mzokovvladimir@users.noreply.github.com>
2023-09-28 23:40:10 +0300

    Update 5.py
    Added type annotation

--- 5.py
+++ 5.py
@@ -1,4 +1,4 @@
-def bmi(weight, height):
+def bmi(weight: int, height: float) -> str:
     bmi = weight / height ** 2
     print(bmi)
     return 'Underweight' if bmi <= 18.5 else 'Normal' if bmi <= 25.0 else 'Overweight' if bmi <= 30.0 else "Obese"
